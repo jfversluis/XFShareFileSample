596403db9a7e3f12c30a1cf9d8680509d107410b
Gerald Versluis <gerald@verslu.is>
2021-09-27 13:13:27 +0200

    Implement sharing

--- XFShareFileSample/MainPage.xaml.cs
+++ XFShareFileSample/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace XFShareFileSample
 {
@@ -13,6 +14,33 @@ namespace XFShareFileSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Subject = "Gerald's YouTube",
+                Text = "Please subscribe",
+                Title = "Gerald's YouTube",
+                Uri = "https://youtube.com/GeraldVersluis"
+            });
+        }
+
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            var image = await MediaPicker.PickPhotoAsync();
+
+            if (image == null)
+            {
+                return;
+            }
+
+            await Share.RequestAsync(new ShareFileRequest
+            {
+                Title = "Gerald's YouTube",
+                File = new ShareFile(image)
+            });
         }
     }
 }
